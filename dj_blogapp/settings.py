a075c13ea576b0b79cb00389b0727c10f6057413
mabbiii <rmuneeb85@gmail.com>
2022-04-17 15:03:14 +0500

    updated to deploy

--- dj_blogapp/settings.py
+++ dj_blogapp/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['django-blogingapp.herokuapp.com', '127.0.0.1']
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
